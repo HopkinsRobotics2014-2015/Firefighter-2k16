42d469d7c75fd9ecfb15ad153198b9c236e67441
tmankendall <taylorwkendall@gmail.com>
2016-01-04 19:56:44 -0500

    much code, such fun. (I hope at least a few of you get that)
    many add ons. Mostly covering what the robot does when it gets to the
    fire. We can talk about which sections should handle the new functions
    on wednesday. Look over all capitalized comments to prepare for
    discussion wednesday.

--- firefighter/movement.ino
+++ firefighter/movement.ino
@@ -1,13 +1,42 @@
+//Need to define this. This is the variable that is multiplied by the distance to get the time the robot needs to move the specified distance
+float Speed;
 
 
 //Returns the distance moved
-float Move(float dist, bool backwards = false){
+float Move(float dist, bool backwards = false) {
+  if (backwards) {
+    pinMode(motorPin, OUTPUT);
+    analogWrite(motorPin, 119);
+    delay(dist * Speed);
+    analogWrite(motorPin, 191);
+  }
+  else {
+    pinMode(motorPin, OUTPUT);
+    analogWrite(motorPin, 240);
+    delay(dist * Speed);
+    analogWrite(motorPin, 191);
+  }
 }
 
 
 //Returns the number of radians turned
+//WHY DOES IT HAVE TO RETURN THE RADIANS TURNED? IT IS TOLD HOW MANY RADIANS IT SHOULD TURN.
 //angle is in radians, since radians make arc length easier
-float Turn(float angle, bool clockwise){
+//DO WE WANT THESE TO HAVE SET VALUES AS SHOWN BELOW OR DO WE WANT TO USE MICE TO DETECT WHEN WE TURN 90 DEGREES EXACTLY
+float Turn(float angle) {
+  float Time;
+  if (angle > 0) {
+    pinMode(motorPin2,OUTPUT);
+    analogWrite(motorPin2,240);
+    delay (angle * Time);
+    analogWrite(motorPin2,119);
+  }
+  else {
+    pinMode(motorPin2,OUTPUT);
+    analogWrite(motorPin2,119);
+    delay(angle*Time);
+    analogWrite(motorPin2,119);
+  }
 }
 
 
@@ -19,5 +48,6 @@ float Turn(float angle, bool clockwise){
 
 //Follows wall until the front sensor can't see the wall anymore
 //returns the distance traveled
-float WallFollow(int wallSide){
+//ARE WE GOING TO RELY ON A DOUBLE MOUSE CONFIG OR ULTRASONICS TO MOVE?
+float WallFollow(int wallSide) {
 }
--- firefighter/navigation.ino
+++ firefighter/navigation.ino
@@ -1,3 +1,5 @@
+//We need pins for the motors
+
 //Handles the actual navigation, not including position checking
 
 class Vec2f
@@ -55,4 +57,51 @@ class Vec2f
       return sqrt(magnitudeSquared());
     }
     
-};
+}
+
+
+
+//Rabbot, i'm gonna leave how you want to call this function to you, but here is a function that asks for scan for fire and then figures out how much the robot needs to turn to
+//face the fire
+
+
+
+void fireTime() {
+// this intializes the sensors in terms of degrees.
+//until we know if the robot can rotate fully in one room, I am coding for five fire sensors so that rotation wont be an issue with this configuration
+  int fireSensor1 = 0;
+  int fireSensor2 = 0;
+  int fireSensor3 = PI/2;
+  int fireSensor4 = PI;
+  int fireSensor5 = -PI/2;
+if (searchForFire()) {
+ int Sensor = fireHere();
+ for (int i = 1; i < 6; i++){
+  if (fireSensor[i] == Sensor){
+    float Sensor = fireSensor[i];
+  }
+ }
+ //HERE IS WHERE WE ROTATE THE ROBOT TO THE FLAME. NEED TO FIGURE OUT IF WE ARE DOING THIS WITH TWO MICE, ULTRASONICS, OR PRE-DETERMINED ROTATION VALUES
+ Turn (-Sensor)
+}
+sprayAndPray()
+}
+
+//This function has the robot slowly approach the flame while blowing the fan.
+
+void sprayAndPray() {
+while (flameSensorRead(0)) {
+  //IM OFFLINE RN BUT FIND FAN COMMAND. RUN FAN AT MAX
+pinMode(motorPin,OUTPUT);
+analogWrite(motorPin,210);
+}
+analogWrite(motorPin,191);
+returnHome();
+}
+
+//This will direct the robot back to the start after the flame has been put out
+void returnHome(){
+  
+}
+
+
