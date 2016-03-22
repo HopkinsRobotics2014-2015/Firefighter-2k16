7c151004bcc8cd9cb281cb528863adc9fc1e8687
akindseth17 <ak47wood@gmail.com>
2016-03-22 13:06:04 -0400

    Removed all the commented out code

--- Test_Firefighter/Test_Firefighter.ino
+++ Test_Firefighter/Test_Firefighter.ino
@@ -1,5 +1,5 @@
 #include <TheLibrary.h>
-
+#include <Servo.h>
 //I KNOW HOW TO FIX IT
 
 
@@ -326,336 +326,6 @@ void determineOrientation()
 
 
 
-// change the hard coded distances to subtract using the checkponts
-// figure out how to communicate with the mouse
-// write out all the instructions for the robot
-// Confirm location with ultrasonics
-
-
-
-
-//Checkpoints Start
-
-
-
-
-
-//Map One Dog 1
-
-
-/*
-void link(int a, int b) {
-  checkpoints[a-1].neighbors.add(checkpoints[b-1]);
-  checkpoints[b-1].neighbors.add(checkpoints[a-1]);
-}
-*/
-//Serves as walls for the map
-/*
-class Line
-{
-  private:
-   pvec3f * start;
-   pvec3f * end;
-  public:
-    Line(Vec3f _start,pvec3f _end) {
-      start = newpvec3f(0, 0);
-      end = newpvec3f(0, 0);
-      start = _start.get();
-      end = _end.get();
-    }
-
-   pvec3f * getStart() {
-      return start;
-    }
-   pvec3f * getEnd() {
-      return end;
-    }
-
-    float setStart(Vec3f * v) {
-      start = v->get();
-    }
-
-    float setEnd(Vec3f * v) {
-      end = v->get();
-    }
-
-    //Returns whether the two line collide
-    bool collides(Line l) {
-     pvec3f * deltaStart = l.getStart()->get();
-      deltaStart->subtract(start);
-     pvec3f * deltaEnd = l.getEnd()->get();
-      deltaEnd->subtract(end);
-      return (deltaStart->getX() * deltaEnd->getX() < 0) &&
-             (deltaStart->getY() * deltaEnd->getY() < 0);
-    }
-};
-*/
-/*
-void visualize() {
-  for (checkpoint cp : checkpoints) {
-    ellipse(map(cp.x, 0, 244, 40, 760), map(cp.y, 0, 244, 40, 560), 20, 20);
-    for (checkpoint neighbor : cp.neighbors) {
-      line(map(cp.x, 0, 244, 40, 760), map(cp.y, 0, 244, 40, 560), map(neighbor.x, 0, 244, 40, 760), map(neighbor.y, 0, 244, 40, 560));
-    }
-  }
-}
-*/
-//checkpoint 1 hard way example
-//THis is assuming i have the checkpoints be values and not variables
-//checkpoints.get(1).neighbors.add(checkpoint.get(2));
-//checkpoint 1 slightly better way using variables for checkpoints
-//ex. checkpoint 1 = new checkpoint (23,42)
-// Checkpoint 1 
-//1.neighbors.add(2);
-
-//Even easier way will be used in this program
-//Use the link function so that you only have to say link(1,2)
-
-//No Walls or Dog code
-/*
- link(1,2);
- link(2,6);
- link(3,4);
- link(4,8);
- link(5,13);
- link(5,6);
- link(6,7);
- link(6,10);
- link(7,8);
- link(7,11);
- link(8,12);
- link(9,10);
- link(9,14);
- link(10,16);
- link(11,12);
- link(12,17);
- link(13,14);
- link(13,15);
- link(15,16);
- link(16,17);
-
- */
-
-//Map One Dog 1
-/*
-void setNeighbors(int map) {
-  
-   for (checkpoint cp : checkpoints){
-    cp.neighbors.clear();
-  }
-  
-  switch (map) {
-
-  case 1:
-  link(1,2);
-  link(2,6);
-  link(3,4);
-  link(4,8);
-  link(5,13);
-  link(5,6);
-  link(6,7);
-  link(6,10);
-  link(7,8);
-  link(7,11);
-  link(8,12);
-  link(9,10);
-  link(9,14);
-  link(10,16);
-  link(11,12);
-  link(12,17);
-  link(13,14);
-  link(13,15);
-  link(15,16);
-  link(16,17);
-  break;
-  // Map One Dog 1
-  case 3:
-    link(1, 2);
-    link(2, 6);
-    link(3, 4);
-    link(4, 8);
-    link(5, 6);
-    link(6, 7);
-    link(6, 10);
-    link(7, 8);
-    link(7, 11);
-    link(9, 10);
-    link(9, 14);
-    link(10, 16);
-    link(11, 12);
-    link(12, 17);
-    link(13, 15);
-    link(15, 16);
-    link(16, 17);
-    break;
-  // Map Two Dog 1
-  case 4:
-    link(1, 2);
-    link(2, 6);
-    link(3, 4);
-    link(4, 8);
-    link(5, 6);
-    link(6, 7);
-    link(6, 10);
-    link(7, 8);
-    link(8, 12);
-    link(9, 10);
-    link(9, 14);
-    link(10, 16);
-    link(11, 12);
-    link(12, 17);
-    link(13, 15);
-    link(15, 16);
-    link(16, 17);
-    break;
-  case 5:
-    // Map 3 dog 1
-    link(1, 2);
-    link(2, 6);
-    link(3, 4);
-    link(4, 8);
-    link(5, 6);
-    link(6, 7);
-    link(6, 10);
-    link(7, 8);
-    link(7, 11);
-    link(9, 14);
-    link(10, 16);
-    link(11, 12);
-    link(12, 17);
-    link(13, 14);
-    link(13, 15);
-    link(15, 16);
-    link(16, 17);
-    break;
-  //Map One Dog 2
-  case 6:
-    link(1, 2);
-    link(2, 6);
-    link(3, 4);
-    link(4, 8);
-    link(5, 13);
-    link(5, 6);
-    link(6, 7);
-    link(6, 10);
-    link(7, 8);
-    link(7, 11);
-    link(9, 10);
-    link(9, 14);
-    link(10, 16);
-    link(11, 12);
-    link(12, 17);
-    link(13, 15);
-    link(16, 17);
-    break;
-  case 7:
-  //Map Two Dog 2
-
-  link(1, 2);
-  link(2, 6);
-  link(3, 4);
-  link(4, 8);
-  link(5, 13);
-  link(5, 6);
-  link(6, 7);
-  link(6, 10);
-  link(7, 8);
-  link(8, 12);
-  link(9, 10);
-  link(9, 14);
-  link(10, 16);
-  link(11, 12);
-  link(12, 17);
-  link(13, 15);
-  link(16, 17);
-  break;
-  case 8: // Map 3 dog 2
-    link(1, 2);
-    link(2, 6);
-    link(3, 4);
-    link(4, 8);
-    link(5, 13);
-    link(5, 6);
-    link(6, 7);
-    link(6, 10);
-    link(7, 8);
-    link(7, 11);
-    link(9, 14);
-    link(10, 16);
-    link(11, 12);
-    link(12, 17);
-    link(13, 14);
-    link(13, 15);
-    link(16, 17);
-    break;
-  //Map One Dog 3
-  case 9:
-    link(1, 2);
-    link(2, 6);
-    link(3, 4);
-    link(4, 8);
-    link(5, 13);
-    link(5, 6);
-    link(6, 7);
-    link(7, 8);
-    link(7, 11);
-    link(9, 10);
-    link(9, 14);
-    link(10, 16);
-    link(11, 12);
-    link(12, 17);
-    link(13, 15);
-    link(15, 16);
-    link(16, 17);
-    break;
-  case 10:
-    // Map 2 Dog 3
-    link(1, 2);
-    link(2, 6);
-    link(3, 4);
-    link(4, 8);
-    link(5, 13);
-    link(5, 6);
-    link(6, 7);
-    link(7, 8);
-    link(8, 12);
-    link(9, 10);
-    link(9, 14);
-    link(10, 16);
-    link(11, 12);
-    link(12, 17);
-    link(13, 15);
-    link(15, 16);
-    link(16, 17);
-    break;
-  case 11:
-    link(1, 2);
-    link(2, 6);
-    link(3, 4);
-    link(4, 8);
-    link(5, 13);
-    link(5, 6);
-    link(6, 7);
-    link(7, 8);
-    link(7, 11);
-    link(9, 14);
-    link(10, 16);
-    link(11, 12);
-    link(12, 17);
-    link(13, 14);
-    link(13, 15);
-    link(15, 16);
-    link(16, 17);
-    break;
-  }
-}
-
-void draw(){
-  //background(255);
-  visualize();
-}
-*/
-
 
 // Checkpoints Start
 int distanceToAlign = 25;
@@ -690,85 +360,9 @@ void Move(float x,float y,float z)
 
 float rotate(float angle)
 {
-  //Here is a proposition
-  /*
-   fullS();
-   takeReads();
-   float angleTurned;
-   float firstReads[6];
-   *firstReads = *reads;
-   float currentReads[6];
-   *currentReads = *reads;
-   float lastRead[6];
-   *lastRead = *reads;
-   float startAngle = currentOrientation.angle;
-   if ( angle == PI/2){
-   fullCW();
-   float lowestRead= 100000;
-   int smallDirection;
-   angleTurned = PI/2;
-   for(int i = 0; i < 6; i++)
-   {
-     if(reads[i] < lowestRead)
-     {
-       lowestRead = reads[i];
-       smallDirection = i;
-     }
-   }
-  while(currentReads[smallDirection] > lastRead[smallDirection])
-  {
-    ultrasonics[smallDirection].takeRead(); 
-  }
-  fullS();
-  fullCW();
-  while(lastRead[smallDirection] > currentReads[smallDirection])
-  {
-    ultrasonics[smallDirection].takeRead();
-  }
-   }
-   if ( angle == -PI/2){
-   fullCCW();
-   float lowestRead= 100000;
-   int smallDirection;
-   angleTurned = -PI/2.0;
-   for(int i = 0; i < 6; i++)
-   {
-     if(reads[i] < lowestRead)
-     {
-       lowestRead = reads[i];
-       smallDirection = i;
-     }
-   }
-  while(currentReads[smallDirection] > lastRead[smallDirection])
-  {
-    ultrasonics[smallDirection].takeRead(); 
-  }
-  fullS();
-  fullCCW();
-  while(lastRead[smallDirection] > currentReads[smallDirection])
-  {
-    ultrasonics[smallDirection].takeRead();
-  }
-   }
-  currentOrientation.angle += angleTurned;
-  return angleTurned;
-  */
+
   fullS();
   takeReads();
-
-  /*
-  if(angle == -PI/2){
-    int primary = minimizeReads();
-    fullCCW();
-    int old = primary;
-    int want = old - 1;
-    if( primary == 0){
-      want = 6;
-    }
-    travel(old, want);
-    
-  }
-  */
 }
 
   void rightpivotback (int value)
@@ -783,27 +377,20 @@ float rotate(float angle)
   STOP();
   alignleft();
 }
+void leftpivotback (int value)
+{
+  takeReads();
+  ultrasoinics[East].takeRead();
+  while (reads[South] > value){
+    ultrasonics[South].takeRead;
+    
+  }
+}
 
   
 
 
-/*
-void callibrate(pvec3f kiwi)
-{
-  //This is the hardest program to write here
-}
-*/
 
-//float CheckDistance (int Pin) {
-//  digitalWrite(Pin, LOW);
-//  delay(2);
-//  digitalWrite(Pin, HIGH);
-//  delay(5);
-//  digitalWrite(Pin, LOW);
-//  pinMode (Pin, INPUT);
-//  digitalWrite(Pin, HIGH);
-//  return pulseIn(Pin, HIGH) / 58.138; //in
-//}
 
 void Align () {
   //take ultrasonic readings for all ultrasonics
@@ -864,185 +451,115 @@ void Align () {
       fullS();
     }
   }
-  /*
-  bool noBack = true;
-  float North = takeRead('N');
-  float East = takeRead('E');
-  float South = takeRead('S');
-  float West = takeRead('W');
-  float first = min (North, East);
-  float second = min (first, South);
-  float Min = min (second, West);
-  if (Min == North) {
-    float one = takeRead('N');
-    fullCCW();
-    delay(1);
-    float two = takeRead('N');
-    while (two <= one) {
-      float two = takeRead('N');
-      noBack = false;
-    }
-    analogWrite(leftMotor, 191);
-    if (noBack == false) {
-      float one = takeRead('N');
-      analogWrite(leftMotor, 240);
-      delay(1);
-      float two = takeRead('N');
-      while (two <= one) {
-      float two = takeRead('N');
-    }
-    analogWrite(leftMotor,191);
-    }
-  }
-  if (Min == East) {
-    float one = takeRead('E');
-    pinMode(leftMotor, OUTPUT);
-    analogWrite(leftMotor, 119);
-    delay(1);
-    float two = takeRead('E');
-    while (two <= one) {
-      float two = takeRead('E');
-      noBack = false;
-    }
-    analogWrite(leftMotor, 191);
-    if (noBack == false) {
-      float one = takeRead('E');
-      analogWrite(leftMotor, 240);
-      delay(1);
-      float two = takeRead('E');
-      while (two <= one) {
-      float two = takeRead('E');
-    }
-    analogWrite(leftMotor,191);
-    }
-  }
-  if (Min == South) {
-    float one = takeRead('S');
-    pinMode(leftMotor, OUTPUT);
-    analogWrite(leftMotor, 119);
-    delay(1);
-    float two = takeRead('S');
-    while (two <= one) {
-      float two = takeRead('S');
-      noBack = false;
-    }
-    analogWrite(leftMotor, 191);
-    if (noBack == false) {
-      float one = takeRead('S');
-      analogWrite(leftMotor, 240);
-      delay(1);
-      float two = takeRead('S');;
-      while (two <= one) {
-      float two = takeRead('S');
-    }
-    analogWrite(leftMotor,191);
-    }
-  }
-  if (Min == West) {
-    float one = takeRead('W');
-    pinMode(leftMotor, OUTPUT);
-    analogWrite(leftMotor, 119);
-    delay(1);
-    float two = takeRead('W');
-    while (two <= one) {
-      float two = takeRead('W');
-      noBack = false;
-    }
-    analogWrite(leftMotor, 191);
-    if (noBack == false) {
-      float one = takeRead('W');
-      analogWrite(leftMotor, 240);
-      delay(1);
-      float two = takeRead('W');
-      while (two <= one) {
-      float two =takeRead('W');
-    }
-    analogWrite(leftMotor,191);
-    }
-  }
-  */
+ 
   takeReads();
 }
 
+
+
+float threshold;
+float light;
+
+Servo Han;
+
+
+void lookForFLame()
+{
+  float reads[6];
+  float lowestRead;
+  int lowestReading;
+  delay(3000);
+  lowestRead = 100000000;
+  
+  for(int i = 40 ; i < 100 ; i+=10)
+  {
+    int p = (i-40)/10.0;
+    Han.write(i);
+    delay(3000);
+    reads[p] = takeFireRead();
+    if(reads[p] < lowestRead)
+    {
+      lowestRead = reads[p];
+      lowestReading = p;
+    }
+  }
+    
+  if( lowestRead < threshold )
+  {
+    putItOut();
+    return;
+  }
+
+  
+  if( lowestRead < light)
+  {
+    moveOnFlame(lowestRead, lowestReading);
+  }
+
+  if(lowestRead > light){
+    return;
+  }
+
+}
+
+
+
+void moveOnFlame(float lowestRead, int lowestReading)
+{    
+  delay(3000);
+  Han.write(65);
+  if((lowestReading*10 + 40) >65);{
+    analogWrite(12, 221);
+    wait(lowestRead);
+    analogWrite(12, 191);
+  }
+  if( (lowestReading*10 + 40) <=65){
+    analogWrite(11, 221);
+    wait(lowestRead);
+    analogWrite(11,191);
+  }
+  
+}
+  
+
+
+
+
+void wait(float lowestRead)
+{
+  while(takeFireRead() > lowestRead){
+    delayMicroseconds(1);
+  }
+}
 //Firefighter Start
-int fireSensorFR = 1;
-int fireSensorFL = 2;
-int fireSensorL = 3;
-int fireSensorB = 4;
-int fireSensorR = 5;
-int fire = 0;
+int firePin = A0;
+int fanPin = 9;
 
 
-
+void putItOut()
+{
+  digitalWrite(9, HIGH);
+}
 //Later on we may want to try a continuous search, but for
 //now navigation will relinquish control to searchForFire()
 
 //returns whether a fire was found
-bool searchForFire(){
-  int i =0;
-      //getting the voltage reading from the temperature sensor
- int reading = analogRead(A0);
- 
- // converting that reading to voltage, for 3.3v arduino use 3.3
- float voltage = reading * 5.0;
- voltage /= 1024.0; 
- 
- // print out the voltage
- //Serial.print(voltage); Serial.println(" volts");
- 
- // now print out the temperature
- Temps[i] = (voltage - 0.5) * 100 ;  //converting from 10 mv per degree wit 500 mV offset. CELCIUS
-                                               //to degrees ((voltage - 500mV) times 100)
- Serial.print("sensor" "A0");
- Serial.print(Temps[i]);
- Serial.println(" degrees C");
-if (Temps[i] > 30) {
- return true;
- fire = 0;
- fireHere();
-    }
- else {
-  return false;
- }
-}
-
-//records which sensor is true
-int fireHere() {
-  return fire;
-}
-
-//This is a function that navigation can call to check one of the flame sensors
-bool flameSensorRead(int i) {
-  int reading = analogRead(A0);
-   float voltage = reading * 5.0;
- voltage /= 1024.0;
- Temps[i] = (voltage - 0.5) * 100 ;
-
-
-
-
- 
- if (Temps[i] > 30) {
-
-
-
-
+bool fireFight()
+{
   
- return true;
- fire = A0;
- fireHere();
-    }
- else {
-  return false;
- }
+  //Take reading
+  delay(500);
 }
 
-void extinguishFire(){
+float takeFireRead()
+{
+  float reading = analogRead(A0);
 }
 
-//Movement start
-//Need to define this. This is the variable that is multiplied by the distance to get the time the robot needs to move the specified distance. Not doing it this way.
-float Speed;
+
+
+
+
 
 
 //Returns the distance moved
